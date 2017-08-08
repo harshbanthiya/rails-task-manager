04537bf1ed8c15af1589e9783a6fbb2c2e053713
harsh banthiya <hbanthiya@gmail.com>
2017-08-08 16:00:22 -0300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
  get    '/tasks',    to:  'tasks#index'
  post   '/tasks',    to:  'tasks#create'
  get    '/tasks/new',to:  'tasks#new'
- get    '/tasks/:id/edit',to:  'tasks#edit'
+ get    '/tasks/:id/edit',to:  'tasks#edit', as: 'edit_task' 
  get    '/tasks/:id',to:  'tasks#show', as: 'task'
  patch  '/tasks/:id',to:  'tasks#update'
  put    '/tasks/:id',to:  'tasks#update'
